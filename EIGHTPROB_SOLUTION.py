5b354769e8d08e74125d6368e16450589ac3d096
Alekhyagnv <58624895+Alekhyagnv@users.noreply.github.com>
2020-01-10 22:32:57 +0530

    Create EIGHTPROB_SOLUTION.py

---
+++ EIGHTPROB_SOLUTION.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the value of X\n"))
+y=int(input("Enter the value of Y\n"))
+c=(y-5*x)//13
+print("Number of children tickets sold :",c) 
+print("Number of adult tickets sold :",c+x)
+print("Number of senior tickets sold :",2*c)
