0f261319299ab43e4247227a9c6bb369e78574ec
Alekhyagnv <58624895+Alekhyagnv@users.noreply.github.com>
2020-01-10 22:30:40 +0530

    Create SEVENTHPROB_SOLUTION.py

---
+++ SEVENTHPROB_SOLUTION.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the total number of people\n"))
+p=2*n/7
+print("Number of attendees on day 1 :",p)
+print("Number of attendees on day 2 :",2*p)
+print("Number of attendees on day 3 :",p//2)
