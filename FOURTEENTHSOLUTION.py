2bca811878bf49890f93640f032075cf3fca4fc1
Alekhyagnv <58624895+Alekhyagnv@users.noreply.github.com>
2020-02-05 22:06:37 +0530

    Create FOURTEENTHSOLUTION.py

---
+++ FOURTEENTHSOLUTION.py
@@ -0,0 +1,12 @@
+a,b,c,d,e=map(int,input().split())
+g=(a+b+c+d+e)/5
+if g>=4.0
+    if (a!=2 and b!=2 and c!=2 and d!=2 and e!=2):
+         if(a==5 or b==5 or c==5 or d==5 or e==5):
+              print("Yes")
+         else:
+               print("No")
+    else:
+         print("No")
+else:
+    print("No")
